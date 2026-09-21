7ba5d2b850c9241a52cc562413122942a76aa2cc
Fangze W <fw24@njit.edu>
2026-09-21 15:16:35 -0400

    Correct subtraction test name #3

--- tests/operations/test_subtract.py
+++ tests/operations/test_subtract.py
@@ -4,7 +4,7 @@ from calculator import subtract
 def test_subtract():
     assert subtract(5, 3) == 2
 
-def test_subtract_negative():
+def test_subtract_negative_result():
     assert subtract(3, 5) == -2
 
 def test_subtract_zero():
