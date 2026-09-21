e09a43b26bdac4dd10599d579ecb444e70ccfa59
Fangze W <fw24@njit.edu>
2026-09-21 15:10:21 -0400

    Implement and test subtraction #3

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -2,3 +2,8 @@ def add(a, b):
     """Return the sum of two numbers."""
     return a + b
     # return 0
+
+def subtract(a, b):
+    """Return the difference of two numbers."""
+    return a - b
+    # return 0
